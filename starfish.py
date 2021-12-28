989b20dcbde0201eef1f26a4a929b1c0d8adebae
Pesti Péter <pestipeti@gmail.com>
2021-12-28 13:03:51 +0100

    Publik (Remek's 0.507) config

--- starfish.py
+++ starfish.py
@@ -14,14 +14,14 @@ class Exp(MyExp):
 
         # ---------------- model config ---------------- #
         self.num_classes = 1
-        self.depth = 1
-        self.width = 1
+        self.depth = 0.33
+        self.width = 0.5
 
         # ---------------- dataloader config ---------------- #
         self.data_num_workers = 8
-        self.input_size = (800, 1280)
-        self.multiscale_range = 10
-        self.random_size = (20, 35)
+        self.input_size = (960, 960)
+        self.multiscale_range = 5
+        self.random_size = (10, 20)
         self.data_dir = None  # You have to add to the end of the train cli
         self.train_ann = "train.json"
         self.val_ann = "val.json"
@@ -40,7 +40,7 @@ class Exp(MyExp):
 
         # --------------  training config --------------------- #
         self.warmup_epochs = 5
-        self.max_epoch = 25
+        self.max_epoch = 20
         self.warmup_lr = 0
         self.basic_lr_per_img = 0.01 / 64.0
         self.scheduler = "yoloxwarmcos"
@@ -50,13 +50,13 @@ class Exp(MyExp):
 
         self.weight_decay = 5e-4
         self.momentum = 0.9
-        self.print_interval = 100
+        self.print_interval = 10
         self.eval_interval = 1
 
         self.exp_name = "sf"
         self.exp_name = str(increment_path(f"YOLOX_outputs/{self.exp_name}", exist_ok=False)).split("/")[-1]
 
         # -----------------  testing config ------------------ #
-        self.test_size = (800, 1280)
+        self.test_size = (960, 960)
         self.test_conf = 0.01
         self.nmsthre = 0.65
